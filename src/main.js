70e72f138e4b8302346bdf8ab093a3a52267b136
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-08-28 15:39:39 +0200

    Initial Vue project setup

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './stores'
+
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.mount('#app')
