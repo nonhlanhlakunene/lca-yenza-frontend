9b1931dbefbff3473550e75f21955dc50f1debcf
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-09-03 09:25:48 +0200

    Added contact page image and contact page route

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    // Contact route
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
+    }
   ],
 })
 
