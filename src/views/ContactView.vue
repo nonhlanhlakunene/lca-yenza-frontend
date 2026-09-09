0d16b72668a0e86074cb8cddd3f42cee64298d97
Nonhlanhla Kunene <knonhlanhla585@gmail.com>
2026-09-09 09:58:30 +0200

    Removed navbar code and it's styling

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,14 +1,4 @@
 <template>
-    <!-- <nav class="home-navbar">
-        <ul class="navbar-links">
-            <li><a href="">Home</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">About Us</a></li>
-            <li><a href="">Reviews</a></li>
-            <li><a href="">Bookings</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </nav> -->
     <div class="contact-page">
         
         <p id="confirm-message" v-if="messageSent">
@@ -159,43 +149,6 @@ export default {
     padding: 0;
     box-sizing: border-box;
 }
-
-/* .home-navbar{
-    background: #136163;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    padding: 15px 50px;
-}
-
-.navbar-links{
-    gap: 50px;
-    display: flex;
-    list-style: none;
-}
-
-.navbar-links a {
-    color: white;
-    text-decoration: none;
-    font-size: 15px;
-    transition: 0.3s;
-}
-
-@media (max-width: 768px){
-    .home-navbar{
-        flex-direction: column;
-    }
-
-    .navbar-links{
-        flex-direction: column;
-        margin: 15px 0;
-        text-align: center;
-    }
-
-    .navbar-links a{
-        margin: 10px 0;
-    }
-} */
 
 .contact-page {
     font-family: var(--font-main);
